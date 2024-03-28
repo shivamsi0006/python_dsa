39cae79ed8299b8fb4972af023202a0b69d57ab6
CA <shivamsi0006@gmail.com>
2024-03-28 18:43:13 +0530

    first commit

---
+++ dictonaries.py
@@ -0,0 +1,33 @@
+
+# dictonaries in python store the elements in key value pairs.
+
+
+# update the key 1 
+
+'''dict_items={
+    "name":"hello",
+    1:["coder","hello","programer"]
+    ,2:56,
+
+    1:56
+}
+
+print(dict_items)'''
+
+# create an empty dictonaries and adding an elements 
+
+'''d1={}
+d1[1]=[1,2,3,4]
+d1[2]=1,2,3,4
+d1["values"]=23
+
+print(d1)'''
+
+# list of tuples 
+'''
+list_tuples=[("a",40),("b",20),("c",10),("d",0)]
+d2=dict(list_tuples)
+print(d2)
+'''
+
+# Accessing the dictionaries items
